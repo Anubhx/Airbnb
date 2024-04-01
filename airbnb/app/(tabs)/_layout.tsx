b7f470e8c556b2e35844a287fd7d2298538e3529
Anubhav Raj <anubhav.raj.srivastav27@gmail.com>
2024-04-01 16:57:36 +0530

    Adding bottom nav buttons files

--- airbnb/app/(tabs)/_layout.tsx
+++ airbnb/app/(tabs)/_layout.tsx
@@ -1,9 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import Colors from '@/constants/Colors';
 
 const Layout = () => {
-  return  <Tabs></Tabs>;
+  return  <Tabs screenOptions={{
+    tabBarActiveTintColor : Colors.primary,
+  }}>
+    <Tabs.Screen name="index" options={{
+        tabBarLabel : 'Explore'
+    }}
+    />
+  </Tabs>;
   
 }
 
---
+++ airbnb/app/(tabs)/inbox.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <View>
+      <Text>Inbox</Text>
+    </View>
+  )
+}
+
+export default Page
